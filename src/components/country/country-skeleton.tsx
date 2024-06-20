c96b6dc326d22877c2c07061d0bf56398acfec68
Raiza <wisdomadebola62@gmail.com>
2024-06-20 02:53:15 +0100

    🔃 Loading skeleton for all countries page built

---
+++ src/components/country/country-skeleton.tsx
@@ -0,0 +1,20 @@
+
+const CountrySkeleton = () => {
+    return (
+        <div className="relative animate-pulse">
+            <div  className="w-full flex flex-col justify-center px-2 md:px-4 border rounded">
+                <div className="flex justify-between items-center border-b border-gray-200 py-1">
+                    <div className="bg-gray-200 h-5 w-36" />
+                    <div className="bg-gray-200 w-20 h-9" />
+                </div>
+
+                <div className="grid grid-cols-1  py-4">
+                    <div className=" flex flex-col  bg-gray-200 h-24 w-full" />      
+                </div>
+
+            </div>
+        </div>
+    )
+} 
+
+export default CountrySkeleton
