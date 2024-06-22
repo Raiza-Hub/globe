d8854cb48adae48c1a96384a647682a1112ab1df
Raiza <wisdomadebola62@gmail.com>
2024-06-22 12:10:51 +0100

    ✒️Interface for currency api

---
+++ src/types/currency.ts
@@ -0,0 +1,15 @@
+interface ExchangeRates {
+    [key: string]: number;
+}
+
+export interface ExchangeData {
+    result: string;
+    documentation: string;
+    terms_of_use: string;
+    time_last_update_unix: number;
+    time_last_update_utc: string;
+    time_next_update_unix: number;
+    time_next_update_utc: string;
+    base_code: string;
+    conversion_rates: ExchangeRates;
+}
