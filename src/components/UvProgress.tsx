cfa91cdb8df391ea0c7dc5fc5902670296a2c9bf
Raiza <wisdomadebola62@gmail.com>
2024-06-26 20:54:50 +0100

    uvprogress component created

---
+++ src/components/UvProgress.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
+
+import { cn } from "@/lib/utils"
+
+const UvProgress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-3 w-full overflow-hidden rounded-full bg-secondary",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-3 w-3 flex-1 bg-primary rounded-full shadow-lg shadow-white ring-2 ring-white text-white"
+      style={{ marginLeft: `calc(${value}% - 0.8rem)` }}
+    />
+  </ProgressPrimitive.Root>
+))
+UvProgress.displayName = ProgressPrimitive.Root.displayName
+
+export { UvProgress }
