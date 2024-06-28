713cf69988aadfc12a7eeb6a538aa4990dc5a6ab
Raiza <wisdomadebola62@gmail.com>
2024-06-28 14:28:34 +0100

    Icons component

---
+++ src/components/Icons.tsx
@@ -0,0 +1,39 @@
+import {
+  CloudDrizzle,
+  CloudRain,
+  Snowflake,
+  CloudSun,
+  Cloudy,
+  Navigation,
+  ThermometerSun,
+  Sunset,
+  Wind,
+  Gauge,
+  Droplets,
+  Thermometer,
+  Eye,
+  UsersRound,
+  CalendarDays,
+  SunDim,
+  CloudLightning,
+  CloudFog,
+} from "lucide-react";
+
+export const drizzleIcon = <CloudDrizzle size={25} />;
+export const thunderStormIcon = <CloudLightning size={25} />;
+export const HazeIcon = <CloudFog size={25} />;
+export const rain = <CloudRain size={30} />;
+export const snow = <Snowflake size={30} />;
+export const clearSky = <CloudSun size={30} />;
+export const cloudy = <Cloudy size={30} />;
+export const navigation = <Navigation size={15} />;
+export const thermo = <ThermometerSun size={15} />;
+export const sunsetIcon = <Sunset size={15} />;
+export const windIcon = <Wind size={15} />;
+export const gauge = <Gauge size={15} />;
+export const droplets = <Droplets size={15} />;
+export const thermometer = <Thermometer size={15} />;
+export const eye = <Eye size={15} />;
+export const people = <UsersRound size={15} />;
+export const calender = <CalendarDays size={15} />;
+export const sun = <SunDim size={15} />;
