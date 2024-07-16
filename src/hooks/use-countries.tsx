ca063f6a64b4310ad02d99f34b3fef1990a9bbc7
Raiza <wisdomadebola62@gmail.com>
2024-07-16 18:22:25 +0100

    added types to the usequery function

--- src/hooks/use-countries.tsx
+++ src/hooks/use-countries.tsx
@@ -1,9 +1,10 @@
 import { allCountries } from "@/action/countries";
+import { Country } from "@/types/countries";
 import { useQuery } from "@tanstack/react-query";
 
 
 export const useCountries = () => {
-    return useQuery({
+    return useQuery<Country[], Error>({
         queryKey: ['countries'],
         queryFn: () => allCountries()
     });
--- src/hooks/use-region.tsx
+++ src/hooks/use-region.tsx
@@ -1,9 +1,10 @@
 import { getRegion } from "@/action/countries";
+import { Country } from "@/types/countries";
 import { useQuery } from "@tanstack/react-query";
 
 
 export const useRegion = (sort: string) => {
-    return useQuery({
+    return useQuery<Country[], Error>({
         queryKey: ['region', sort],
         queryFn: () => getRegion(sort)
     });
