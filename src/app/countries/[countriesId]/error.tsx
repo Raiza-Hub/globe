45e79c634df01d3e2ee4046a9eac33a8b45ce315
Raiza <wisdomadebola62@gmail.com>
2024-07-16 22:10:07 +0100

    error component working ryt

--- src/app/countries/[countriesId]/error.tsx
+++ src/app/countries/[countriesId]/error.tsx
@@ -1,34 +1,60 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button"
 import Image from "next/Image"
+import Link from "next/link";
 
-export default function ErrorBoundary({ error }: {
-    error: Error
-}) {
+export default function ErrorBoundary({ error }: { error: Error }) {
+    const [isOffline, setIsOffline] = useState(!navigator.onLine);
+
+    useEffect(() => {
+        const updateOnlineStatus = () => {
+            setIsOffline(!navigator.onLine);
+        };
+
+        window.addEventListener('online', updateOnlineStatus);
+        window.addEventListener('offline', updateOnlineStatus);
+
+        return () => {
+            window.removeEventListener('online', updateOnlineStatus);
+            window.removeEventListener('offline', updateOnlineStatus);
+        };
+    }, []);
+
     return (
         <div className='w-full flex flex-col justify-center items-center px-4 mt-8 lg:max-w-7xl lg:mx-auto lg:px-8 lg:mt-12'>
             <div className='flex flex-col items-center gap-8'>
                 <Image
-                    src="/No connection-bro.svg"
+                    src={isOffline ? "/No connection-bro.svg" : "/not-found.svg"}
                     alt={"The world map"}
                     width={300}
                     height={300}
                     className="w-62 h-62 mr-4 object-cover object-center"
                 />
-                <p className='max-w-prose text-lg font-bold'>
-                    Something went wrong.
-                </p>
+                <h1 className='max-w-prose text-2xl font-bold'>
+                    {isOffline ? "No internet connection." : "Page not Found."}
+                </h1>
+                <p className='hidden'>{error.message}</p>
             </div>
-            <Button
-            variant='link'
-            className='pt-2 font-medium text-gray-700 dark:text-white'
-            onClick={() => {
-                window.location.reload();
-            }}
+            {isOffline ? (
+                <Button
+                variant='link'
+                className='pt-2 font-medium text-gray-700 dark:text-white text-lg'
+                onClick={() => {
+                    window.location.reload();
+                }}
             >
                 try again
             </Button>
+            ) : (
+                <Link
+                href='/'
+                className='pt-2 font-medium text-gray-700 dark:text-white hover:underline'
+            >
+                Back to homepage
+            </Link>
+            )}
         </div>
     )
 }
