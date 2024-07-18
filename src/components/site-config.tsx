344438e79b5360113d737a3d2f569dbf227de1a0
Raiza <wisdomadebola62@gmail.com>
2024-07-18 17:16:44 +0100

    site config detail

---
+++ src/components/site-config.tsx
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Globe",
+    url: "http://localhost:3000",
+    ogImage: "/logo.png",
+    description:
+      "Discover valuable insights into various Climates, Countries and Currencies",
+    links: {
+      twitter: "https://x.com/wisdom_adebola",
+      github: "https://github.com/Raiza-Hub/globe",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
